1fe846584efa41b2a241a3a66451538a7cef309d
Albert Akhmetov <albert.akhmetov@hotmail.com>
2016-03-27 21:54:28 +0500

    documentation

--- sources/LiteRepository/Db.cs
+++ sources/LiteRepository/Db.cs
@@ -26,12 +26,20 @@ using Dapper;
 
 namespace LiteRepository
 {
+    /// <summary>
+    /// Provides functional to execute queries for database
+    /// </summary>
     public class Db
     {
         private readonly SqlDialect _sqlDialect;
         private readonly DbConnection _dbConnection;
         private readonly Func<DbConnection> _dbConnectionFactory;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Db"/> class
+        /// </summary>
+        /// <param name="sqlDialect">The <see cref="SqlDialect"/> instance</param>
+        /// <param name="dbConnection">The <see cref="DbConnection"/> instance</param>
         public Db(SqlDialect sqlDialect, DbConnection dbConnection)
         {
             if (sqlDialect == null)
@@ -44,6 +52,11 @@ namespace LiteRepository
             _dbConnectionFactory = null;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Db"/> class
+        /// </summary>   
+        /// <param name="sqlDialect">The <see cref="SqlDialect"/> instance</param>
+        /// <param name="dbConnectionFactory">The callback function for creating instances of <see cref="DbConnection"/></param>
         public Db(SqlDialect sqlDialect, Func<DbConnection> dbConnectionFactory)
         {
             if (sqlDialect == null)
@@ -56,6 +69,12 @@ namespace LiteRepository
             _dbConnectionFactory = dbConnectionFactory;
         }
 
+        /// <summary>
+        /// Executes any <paramref name="action"/> method in database transaction.
+        /// </summary>
+        /// <typeparam name="T">Return type.</typeparam>
+        /// <param name="action">Callback method to execute.</param>
+        /// <returns>Result of the <paramref name="action"/> execute.</returns>
         public virtual T Exec<T>(Func<DbConnection, T> action)
         {
             var isOpened = _dbConnection?.State == System.Data.ConnectionState.Open;
@@ -77,6 +96,12 @@ namespace LiteRepository
             }
         }
 
+        /// <summary>
+        /// An asynchronous version of <see cref="Exec{T}">Exec</see>
+        /// </summary>
+        /// <typeparam name="T">Return type.</typeparam>
+        /// <param name="action">Callback method to execute.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
         public virtual async Task<T> ExecAsync<T>(Func<DbConnection, Task<T>> action)
         {
             var isOpened = _dbConnection?.State == System.Data.ConnectionState.Open;
@@ -103,6 +128,12 @@ namespace LiteRepository
             return new SqlExpression<E>(_sqlDialect);
         }
 
+        /// <summary>
+        /// Inserts <paramref name="entity"/> to the database
+        /// </summary>
+        /// <typeparam name="E">Type of the entity</typeparam>
+        /// <param name="entity">Entity to insert into the database</param>
+        /// <returns>If entity is identity - returns a new entity with a new Id. Otherwise it returns original <paramref name="entity"/>.</returns>
         public virtual E Insert<E>(E entity) where E : class
         {
             if (entity == null)
@@ -124,6 +155,12 @@ namespace LiteRepository
             });
         }
 
+        /// <summary>
+        /// An asynchronous version of <see cref="Insert{E}">Insert</see>
+        /// </summary>
+        /// <typeparam name="E">Type of the entity</typeparam>
+        /// <param name="entity">Entity to insert into the database</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
         public virtual Task<E> InsertAsync<E>(E entity) where E : class
         {
             if (entity == null)
@@ -145,6 +182,12 @@ namespace LiteRepository
             });
         }
 
+        /// <summary>
+        /// Updates <paramref name="entity"/> in the database
+        /// </summary>
+        /// <typeparam name="E">Type of the entity</typeparam>
+        /// <param name="entity">Entity to update in database</param>
+        /// <returns>Count of a effected enities.</returns>
         public virtual int Update<E>(E entity) where E : class
         {
             if (entity == null)
@@ -154,6 +197,12 @@ namespace LiteRepository
             return Exec(dbConnection => dbConnection.Execute(sqlExpression.GetUpdateSql(), entity));
         }
 
+        /// <summary>
+        /// An asynchronous version of <see cref="Update{E}(E)">Update</see>
+        /// </summary>
+        /// <typeparam name="E">Type of the entity</typeparam>
+        /// <param name="entity">Entity to update in the database</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
         public virtual Task<int> UpdateAsync<E>(E entity) where E : class
         {
             if (entity == null)
@@ -163,6 +212,13 @@ namespace LiteRepository
             return ExecAsync(dbConnection => dbConnection.ExecuteAsync(sqlExpression.GetUpdateSql(), entity));
         }
 
+        /// <summary>
+        /// Updates subset <paramref name="subEntity"/> of the entity in the database
+        /// </summary>
+        /// <typeparam name="E">Type of the entity</typeparam>
+        /// <param name="subEntity">Entity to update in database</param>
+        /// <param name="where">Where expression.</param>
+        /// <returns>Count of a effected enities.</returns>
         public virtual int Update<E>(object subEntity, Expression<Func<E, bool>> where) where E : class
         {
             if (subEntity == null)
@@ -175,6 +231,13 @@ namespace LiteRepository
             return Exec(dbConnection => dbConnection.Execute(sql, subEntity));
         }
 
+        /// <summary>
+        /// An asynchronous version of <see cref="Update{E}(object, Expression{Func{E, bool}})">Update</see>
+        /// </summary>
+        /// <typeparam name="E">Type of the entity</typeparam>
+        /// <param name="subEntity">Entity to update in database</param>
+        /// <param name="where">Where expression.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
         public virtual Task<int> UpdateAsync<E>(object subEntity, Expression<Func<E, bool>> where) where E : class
         {
             if (subEntity == null)
@@ -187,6 +250,13 @@ namespace LiteRepository
             return ExecAsync(dbConnection => dbConnection.ExecuteAsync(sql, subEntity));
         }
 
+        /// <summary>
+        /// Deletes entity with <paramref name="key"/> from the database
+        /// </summary>
+        /// <typeparam name="E">Type of the entity</typeparam>
+        /// <typeparam name="K">Type of the key</typeparam>
+        /// <param name="key">Key of the entity</param>
+        /// <returns>Count of a effected enities.</returns>
         public virtual int Delete<E, K>(K key) where E : class, K where K : class
         {
             if (key == null)
@@ -197,6 +267,13 @@ namespace LiteRepository
             return Exec(dbConnection => dbConnection.Execute(sql, key));
         }
 
+        /// <summary>
+        /// An asynchronous version of <see cref="Delete{E, K}(K)">Delete</see>
+        /// </summary>
+        /// <typeparam name="E">Type of the entity</typeparam>
+        /// <typeparam name="K">Type of the key</typeparam>
+        /// <param name="key">Key of the entity</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
         public virtual Task<int> DeleteAsync<E, K>(K key) where E : class, K where K : class
         {
             if (key == null)
@@ -207,6 +284,13 @@ namespace LiteRepository
             return ExecAsync(dbConnection => dbConnection.ExecuteAsync(sql, key));
         }
 
+        /// <summary>
+        /// Deletes entities which meet <paramref name="where"/> conditions from the database
+        /// </summary>
+        /// <typeparam name="E">Type of the entity</typeparam>
+        /// <param name="where">Where expression.</param>
+        /// <param name="param">Query parameters</param>
+        /// <returns>Count of a effected enities.</returns>
         public virtual int Delete<E>(Expression<Func<E, bool>> where, object param = null) where E : class
         {
             if (where == null)
@@ -217,6 +301,13 @@ namespace LiteRepository
             return Exec(dbConnection => dbConnection.Execute(sql, param));
         }
 
+        /// <summary>
+        /// An asynchronous version of <see cref="Delete{E}(Expression{Func{E, bool}}, object)">Delete</see>
+        /// </summary>
+        /// <typeparam name="E">Type of the entity</typeparam>
+        /// <param name="where">Where expression.</param>
+        /// <param name="param">Query parameters</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
         public virtual Task<int> DeleteAsync<E>(Expression<Func<E, bool>> where, object param = null) where E : class
         {
             if (where == null)
@@ -227,6 +318,10 @@ namespace LiteRepository
             return ExecAsync(dbConnection => dbConnection.ExecuteAsync(sql, param));
         }
 
+        /// <summary>
+        /// Truncates entities from the database
+        /// </summary>
+        /// <typeparam name="E">Type of the entity</typeparam>
         public virtual void Truncate<E>() where E : class
         {
             var sqlExpression = GetSqlExpression<E>();
@@ -234,6 +329,11 @@ namespace LiteRepository
             Exec(dbConnection => dbConnection.Execute(sql));
         }
 
+        /// <summary>
+        /// An asynchronous version of <see cref="Truncate{E}">Delete</see>
+        /// </summary>
+        /// <typeparam name="E">Type of the entity</typeparam>
+        /// <returns>A task representing the asynchronous operation.</returns>
         public virtual Task TruncateAsync<E>() where E : class
         {
             var sqlExpression = GetSqlExpression<E>();
@@ -241,6 +341,14 @@ namespace LiteRepository
             return ExecAsync(dbConnection => dbConnection.ExecuteAsync(sql));
         }
 
+        /// <summary>
+        /// Returns an entity with the <paramref name="key"/>
+        /// </summary>
+        /// <typeparam name="E">Type of the entity</typeparam>
+        /// <typeparam name="K">Type of the entity key</typeparam>
+        /// <param name="key">Entity key</param>
+        /// <param name="type">Type which sets a subsets of the fields to retrive</param>
+        /// <returns></returns>
         public virtual E GetByKey<E, K>(K key, Type type = null) where E : class, K where K : class
         {
             if (key == null)
@@ -251,6 +359,7 @@ namespace LiteRepository
             return Exec(dbConnection => dbConnection.Query<E>(sql, key).FirstOrDefault());
         }
 
+        /// <returns>A task representing the asynchronous operation.</returns>
         public virtual Task<E> GetByKeyAsync<E, K>(K key, Type type = null) where E : class, K where K : class
         {
             if (key == null)
@@ -261,6 +370,15 @@ namespace LiteRepository
             return ExecAsync(async dbConnection => (await dbConnection.QueryAsync<E>(sql, key)).FirstOrDefault());
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="E"></typeparam>
+        /// <param name="type"></param>
+        /// <param name="where"></param>
+        /// <param name="param"></param>
+        /// <param name="orderBy"></param>
+        /// <returns></returns>
         public virtual IEnumerable<E> Get<E>(
             Type type = null,
             Expression<Func<E, bool>> where = null,
@@ -272,6 +390,7 @@ namespace LiteRepository
             return Exec(dbConnection => dbConnection.Query<E>(sql, param));
         }
 
+        /// <returns>A task representing the asynchronous operation.</returns>
         public virtual Task<IEnumerable<E>> GetAsync<E>(
             Type type = null,
             Expression<Func<E, bool>> where = null,
@@ -283,6 +402,15 @@ namespace LiteRepository
             return ExecAsync(dbConnection => dbConnection.QueryAsync<E>(sql, param));
         }
 
+        /// <summary>
+        /// Returns a scalar expression
+        /// </summary>
+        /// <typeparam name="E"></typeparam>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression"></param>
+        /// <param name="where"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
         public virtual T GetScalar<E, T>(
             Expression<Func<IEnumerable<E>, T>> expression,
             Expression<Func<E, bool>> where = null,
@@ -296,6 +424,7 @@ namespace LiteRepository
             return Exec(dbConnection => dbConnection.ExecuteScalar<T>(sql, param));
         }
 
+        /// <returns>A task representing the asynchronous operation.</returns>
         public virtual Task<T> GetScalarAsync<E, T>(
             Expression<Func<IEnumerable<E>, T>> expression,
             Expression<Func<E, bool>> where = null,
--- sources/LiteRepository/SqlMetadata.cs
+++ sources/LiteRepository/SqlMetadata.cs
@@ -26,6 +26,9 @@ using System.Threading.Tasks;
 
 namespace LiteRepository
 {
+    /// <summary>
+    /// Provides functionality for extracting metadata from type definition
+    /// </summary>
     public sealed class SqlMetadata : IEnumerable<SqlMetadata.Property>
     {
         #region Metadata's cache
@@ -44,28 +47,50 @@ namespace LiteRepository
 
         #endregion
 
+        /// <summary>
+        /// Defines item of metadata information
+        /// </summary>
         public sealed class Property
         {
+            /// <summary>
+            /// Gets a name of the property
+            /// </summary>
             public string Name
             {
                 get; private set;
             }
 
+            /// <summary>
+            /// Gets a name of the database column
+            /// </summary>
             public string DbName
             {
                 get; private set;
             }
 
+            /// <summary>
+            /// Gets a value indicating whether the property is a part of the primary key 
+            /// </summary>
             public bool IsPrimaryKey
             {
                 get; private set;
             }
 
+            /// <summary>
+            /// Gets a value indicating whether the property is identity primary key
+            /// </summary>
             public bool IsIdentity
             {
                 get; private set;
             }
 
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Property"/> class
+            /// </summary>
+            /// <param name="name">The name of the property.</param>
+            /// <param name="dbName">The name of the database column.</param>
+            /// <param name="isPrimaryKey">Determines whether a property is a part of the primary key</param>
+            /// <param name="isIdentity">Determines whether a property is a identity key.</param>
             public Property(string name, string dbName, bool isPrimaryKey, bool isIdentity)
             {
                 Name = name;
@@ -78,41 +103,70 @@ namespace LiteRepository
         private readonly IList<Property> _properties;
         private readonly Dictionary<string, string> _nameToDbNameDictionary;
 
+        /// <summary>
+        /// Gets a type of the entity
+        /// </summary>
         public Type Type
         {
             get; private set;
         }
 
+        /// <summary>
+        /// Gets a name of the entity
+        /// </summary>
         public string Name
         {
             get; private set;
         }
 
+        /// <summary>
+        /// Gets a database table name
+        /// </summary>
         public string DbName
         {
             get; private set;
         }
 
+        /// <summary>
+        /// Gets a value indicating whether the table is the table with identity primary key
+        /// </summary>
         public bool IsIdentity
         {
             get; private set;
         }
 
+        /// <summary>
+        /// Gets a property information by <paramref name="index"/>.
+        /// </summary>
+        /// <param name="index">Index of property information</param>
+        /// <returns></returns>
         public Property this[int index]
         {
             get { return _properties[index]; }
         }
 
+        /// <summary>
+        /// Gets a property information by <paramref name="name"/>.
+        /// </summary>
+        /// <param name="name">Name of the property</param>
+        /// <returns></returns>
         public string this[string name]
         {
             get { return _nameToDbNameDictionary[name]; }
         }
 
+        /// <summary>
+        /// Gets a count of properties
+        /// </summary>
         public int Count
         {
             get { return _properties.Count; }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SqlMetadata"/> class
+        /// </summary>
+        /// <param name="type">Type of the entity</param>
         public SqlMetadata(Type type)
         {
             if (type == null)
@@ -156,15 +210,24 @@ namespace LiteRepository
             _nameToDbNameDictionary = _properties.ToDictionary(i => i.Name, i => i.DbName);
         }
 
+        /// <summary>
+        /// Gets a collection of property information only for properties which contains in the <paramref name="type"/>.
+        /// </summary>
+        /// <param name="type">Type what represents a subset of the entity</param>
+        /// <returns>Collection of <see cref="Property"/>.</returns>
         public IEnumerable<Property> GetSubsetForType(Type type)
         {
             if (type == null)
                 throw new ArgumentNullException(nameof(type));
-        
+
             var typeProperties = type.GetProperties().Select(i => i.Name);
             return _properties.Where(i => typeProperties.Contains(i.Name));
         }
 
+        /// <summary>
+        /// Returns a enumerator
+        /// </summary>
+        /// <returns></returns>
         public IEnumerator<Property> GetEnumerator()
         {
             return _properties.GetEnumerator();
@@ -175,16 +238,30 @@ namespace LiteRepository
             return GetEnumerator();
         }
 
+        /// <summary>
+        /// Compares <paramref name="obj"/> and current instance
+        /// </summary>
+        /// <param name="obj">Another instance of the metadata to compare</param>
+        /// <returns>True if <paramref name="obj"/> is equals to the current metadata</returns>
         public override bool Equals(object obj)
         {
             return Equals(obj as SqlMetadata);
         }
 
+        /// <summary>
+        /// Compares <paramref name="metadata"/> and current instance
+        /// </summary>
+        /// <param name="metadata">Another instance of the metadata to compare</param>
+        /// <returns>True if <paramref name="metadata"/> is equals to the current metadata</returns> 
         public bool Equals(SqlMetadata metadata)
         {
             return metadata != null && (metadata.Type == Type);
         }
 
+        /// <summary>
+        /// Returns the Hash Code for this instance
+        /// </summary>
+        /// <returns>The Hash Code for this instance</returns>
         public override int GetHashCode()
         {
             return Type.GetHashCode();
