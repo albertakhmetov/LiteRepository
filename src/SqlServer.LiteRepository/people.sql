5c40a1845eda3b9e8102a7af1e076f7e2ebcbb26
Albert Akhmetov <akhmetov@live.com>
2025-01-13 17:19:37 +0500

    Add the sources

---
+++ src/SqlServer.LiteRepository/people.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[people]
+(
+	[id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [first_name] NVARCHAR(80) NOT NULL, 
+    [second_name] NVARCHAR(80) NOT NULL, 
+    [birthday] DATE NOT NULL 
+)
